2e0a930a59efb76f8dbd98c94d24005adb7a969e
Saifur Rahman <saifurrahman24to7@gmail.com>
2025-03-02 22:58:58 +0600

    added another page

--- login.js
+++ login.js
@@ -1,3 +1,16 @@
 document.getElementById('login-btn').addEventListener('click', function (event) { 
   event.preventDefault();
+
+  const accountNumber = document.getElementById("account-number").value;
+  const pin = document.getElementById('pin').value;
+  const convertedPin = parseInt(pin)
+  if(accountNumber.length === 9){
+    if (convertedPin === 1234){
+      window.location.href = 'main/index.html'
+    }else{
+      alert('pin thick nai')
+    }
+  }else{
+    alert('need vaild account number');
+  }
 })
